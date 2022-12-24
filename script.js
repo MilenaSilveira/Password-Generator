4362081d4d7070f93773449b7b168c2888d1bf62
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2022-12-24 00:13:41 -0500

    added given code and changed var

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+var generateBtn = document.querySelector("#generate");
 
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Sz"];
 const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -6,39 +7,46 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 const specialChar = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "-", "_", "/", ".", ",", "'", ";", ":", "[","]","{", "}", "?", "<", ">", "|",]
 
 // Assignment Code
-function getPasswordOption(){
+function generatePassword(){
 
   var addOptions = []
-  var finalPassword = []
-  var passwordLength = []
+  var finalPassword = ""
+  var passwordLength = prompt ("How long would you like your password to be? ")
 
+  if (passwordLength <= 8 && passLength >= 128) {
+  
+     alert ("Please create a password with a length between 8 and 128 characters. ")
+     generatePassword();
+    }
+
+else{
   var wantSpecial = confirm("Would you like to add Special Characters?")
   var wantLowerCased = confirm("Would you like to add Lower Case letters?")
   var wantUpperCased = confirm("Would you like to add Upper Case letters?")
   var wantNumbers = confirm("Would you like to add Numbers?")
   
-}
+
 
 if (wantSpecial) {
   addOptions = addOptions.concat(specialChar)
 
 }
 if (wantLowerCased) {
-  addOptions = addOptions.concat(lowerCasedChar)
+  addOptions = addOptions.concat(lowerCase)
 
 }
 if (wantUpperCased) {
-  addOptions = addOptions.concat(upperCasedChar)
+  addOptions = addOptions.concat(upperCase)
 
 }
 if (wantNumbers) {
-  addOptions = addOptions.concat(numericalChar)
+  addOptions = addOptions.concat(numbers)
 
 }
 
 if (wantSpecial == false && wantLowerCased == false && wantUpperCased == false && wantNumbers == false){
   alert ("You must choose one type of character for your password")
-  getPasswordOption()
+  generatePassword()
 }
 if (passwordLength >= 8 && passLength <= 128) {
     for(var i=0; i < passLength; i++) {
@@ -46,7 +54,19 @@ if (passwordLength >= 8 && passLength <= 128) {
       finalPassword += addOptions[randomPassword]
     }
 }
+}
+document.getElementById("password").innerHTML = finalPassword
+
+}
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
